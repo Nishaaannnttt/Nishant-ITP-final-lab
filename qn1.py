66a26b1c512d7f215be5e302885602cfac4253a8
Nishant <bhusalnishant93@gmail.com>
2023-04-18 11:18:46 -0400

    made the final changes

--- qn1.py
+++ qn1.py
@@ -2,8 +2,8 @@
 print("WELCOME TO THE AMANDO SHOPPING SITE")
 print("1. Add product to the cart. ")
 print("2. Search the product. ")
-print("1. Delete the product from the cart. ")
-print("1. Quit. ")
+print("3. Delete the product from the cart. ")
+print("4. Quit. ")
 
 shoppingCart={}   # an empty dictionary
 
@@ -12,25 +12,25 @@ while True:    # to run program in loop
 
     if option==1:       # giving condition to add the product when the customers inputs option 1
         x=int(input("Enter the nuumber of items to be added in the stationary: "))  # asling for the no of items to be added
-        i=0
-        while i<x:       # providing a condition to iterate the program in accordance to the no of items user wants to add
-            if len(shoppingCart)>=5:       # when user inputs more than 5 items to add it prints this 
+        
+        for i in range (x):     # providing a condition to iterate the program in accordance to the no of items user wants to add
+            if len(shoppingCart)==5:       # when user inputs more than 5 items to add it prints this 
                 print("Cart is full. ")
                 
             y=input("Enter an item: ")   #for key 
             z=input("Enter the brand name: ")   # for value
                 
                 # to add the key-value pair in empty dict
-            # update_shoppingCart=({y:z})
-            # shoppingCart.update(update_shoppingCart)  #updated the empty dict in new dict adding all its previous values
+            update_shoppingCart=({y:z})
+            shoppingCart.update(update_shoppingCart)  #updated the empty dict in new dict adding all its previous values
+            
+            print("You added the following items to the cart: ")    
+            for key, value in shoppingCart.items():
+                print("{}:{}".format(key,value))         # prints the dictionary items as we want wihtout brackets and quotations
                 
-            print("You added the following items to the cart: ")
-            for keys, values in shoppingCart.items():
-                print("{}:{}".format(keys,values))         # prints the dictionary items as we want wihtout brackets and quotations
-   
-            
-        i=i+1
-            
+            # print("You added the following items to the cart: ")
+            # print(update_shoppingCart)
+      
 
     elif option==2:   #condition to search
         a=input("Enter the item to be searched: ")
@@ -38,10 +38,23 @@ while True:    # to run program in loop
             print(y,":",z)     # it prints this
         else:
             print("No product exits with this name.")   # if its not in key then it prints this 
-            
-    elif option==3:
-        product=input('Enter the product to be deleted: ')
-        if product in y:
+       
+              
+    elif option==3:         # to delete the product
+        product=input('Enter the product to be deleted: ')      
+        if product in shoppingCart:     # to check if product is in key of dict
+            del shoppingCart[product]    # to delte the product
+        if y==0:
+            print("Cart is empty. No items is found ")
+       
+                
+    elif option==4:
+        break                  # this breaks the loop
+    
+    else:
+        print("Wrong option entered.")
+                
+    
                         
             
             
