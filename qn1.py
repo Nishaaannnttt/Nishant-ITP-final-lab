dcceebe944ae077a4522f0db1708a7bd35000d0d
Nishant <bhusalnishant93@gmail.com>
2023-04-18 08:38:22 -0400

    made the first changes

---
+++ qn1.py
@@ -0,0 +1,6 @@
+#   PRINTING AS PER THE OUTPUT GIVEN 
+print("WELCOME TO THE AMANDO SHOPPING SITE")
+print("1. Add product to the cart. ")
+print("2. Search the product. ")
+print("1. Delete the product from the cart. ")
+print("1. Quit. ")
